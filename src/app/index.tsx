d74b45c8c5efde4dbdf7a1a588551b78f3b22617
Shittu Emmanuel <shem4soul@gmail.com>
2026-08-16 06:59:19 +0100

    fix a bug

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
+
+      <Link href="/about" style={{ marginTop: 20 }}>
+        Go to About
+      </Link>
     </View>
   );
 }
